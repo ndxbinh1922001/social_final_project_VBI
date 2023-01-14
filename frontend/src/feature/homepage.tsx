8aaeecb52f2aafb73e39570f328a59bb9f4cc695
ndxbinh1922001 <binhndx1922001@gmail.com>
2023-01-13 21:39:18 -0800

    fixbug frontend

--- frontend/src/feature/homepage.tsx
+++ frontend/src/feature/homepage.tsx
@@ -8,16 +8,11 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 interface IHomePageProps {}
 const HomePage: React.FunctionComponent<IHomePageProps> = (props) => {
-<<<<<<< HEAD
-  // export default function HomePage() {
-=======
->>>>>>> e1dd4b1c8762bba391c735c7ea0ae1e1fab8fa94
+
   const { user } = useContext(AuthContext);
   return (
     <>
-    
        <Topbar />
-      
       <div className="homeContainer">
         <Sidebar />
         <Feed username="" />
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -2,10 +2,7 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "../feature/homepage";
 import Register from "../feature/register";
 import Login from "../feature/Login";
-<<<<<<< HEAD
 import Profile from "../feature/Profile";
-=======
->>>>>>> e1dd4b1c8762bba391c735c7ea0ae1e1fab8fa94
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 function Router() {
@@ -14,7 +11,6 @@ function Router() {
   
   return (
     <Routes>
-<<<<<<< HEAD
       <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/" element= {user !== null ? <HomePage /> : <Navigate to="/login" />} /> 
@@ -22,11 +18,6 @@ function Router() {
           path="/profile/:username"
           element={user ? <Profile /> : <Navigate to="/login" />}
         />
-=======
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/" element={<HomePage />} />
->>>>>>> e1dd4b1c8762bba391c735c7ea0ae1e1fab8fa94
     </Routes>
   );
 }
